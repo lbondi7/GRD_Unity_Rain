1e42744eba71f88d6663e2824bae432b59e50fe9
David K <davek032@outlook.com>
2021-01-26 17:34:54 +0000

    Added starting content
    Added early prototype + Polygon free demo + Unity Standard Particles

---
+++ Assets/3rdPersonCharacter/PlayerControls.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    Vector2 move;
+    Vector2 look;
+    Animator playerAnimator;
+    GameObject playerCameraObj;
+    public Transform playerRoot;
+
+    bool amCrouching = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerCameraObj = GetComponentInChildren<Camera>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControlUpdate();
+    }
+    void ControlUpdate() 
+    {
+        playerCameraObj.transform.RotateAround
+            (playerRoot.position, Vector3.up, look.x / 1.5f);
+        playerAnimator.SetFloat("Turn", move.x);
+        playerAnimator.SetFloat("Forward", move.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+        //Do a flip here?
+    }
+
+    public void OnLook(InputAction.CallbackContext context) 
+    {
+        look = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context) 
+    {
+    
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context) 
+    {
+        amCrouching = !amCrouching;
+
+        if (amCrouching)
+        {
+            playerAnimator.SetBool("Crouch", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("Crouch", false);
+        }
+    }
+
+
+}
---
+++ Assets/RainControls/BasicRain.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RainShape
+{
+    Box,
+    Cylinder,
+    Plane
+}
+
+public class BasicRain : MonoBehaviour
+{
+    public RainShape Shape = RainShape.Box;
+    [Header("Size of Rain Source")]
+    [Range(0, 10)]
+    public int RainSize = 2;
+    [Header("Cloud")]
+    public GameObject CloudObject;
+    public bool SpawnCloud = true;
+
+    GameObject cloud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SpawnCloud)
+        {
+            cloud = Instantiate(CloudObject, transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnValidate()
+    {
+        var rainData = GetComponent<ParticleSystem>().shape;
+        switch (Shape)
+        {
+            case RainShape.Box:
+                rainData.shapeType = ParticleSystemShapeType.Box;
+                break;
+            case RainShape.Cylinder:
+                rainData.shapeType = ParticleSystemShapeType.Cone;
+                rainData.angle = 0;
+                rainData.length = 1;
+                break;
+            case RainShape.Plane:
+                break;
+            default:
+                break;
+        }
+        rainData.scale = new Vector3(RainSize, RainSize, 1);
+        CloudConfig();
+    }
+
+    void CloudConfig() 
+    {
+        if (!cloud.GetComponent<ParticleSystem>())
+        {
+            return;
+        }
+
+        var cloudData = cloud.GetComponent<ParticleSystem>().shape;
+        cloudData.radius = RainSize;
+    }
+}
