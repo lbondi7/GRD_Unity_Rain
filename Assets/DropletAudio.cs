38a7d0c0bf8a1fb9a769265a3f9b6d1b780c0bc2
mudkipz5 <mudkipz5@yahoo.co.uk>
2021-02-22 21:15:00 +0000

    Lots of audio things. One shots working

--- Assets/DropletAudio.cs
+++ Assets/DropletAudio.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class DropletAudio : MonoBehaviour
 {
-    public string Event = "";
-    
+    string Wood = "event:/Impacts/Wood";
+    string Rock = "event:/Impacts/Rock";
+    string Water = "event:/Impacts/Water";
+    string Grass = "event:/Impacts/Grass";
+
+    string Material;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlaySound();
+    }
+
+    void PlaySound()
+    {
+        Vector3 newPos = new Vector3(7.78f, 11.68f, -5.5f);
+        FMODUnity.RuntimeManager.PlayOneShot(Material, newPos);
     }
 }
